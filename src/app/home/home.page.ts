6d6dcf18d51b6f564ceb36d85116a2fa39f90674
Juan Carlos Vargas Chavez <jcvargas273@gmail.com>
2023-10-29 14:14:21 +0000

    Encapsulacion de appWeb

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.openWebPage();
+
+    // Browser.addListener('browserFinished', () => {
+    //   console.log('Browser finished');
+    //   App.exitApp();
+    // });
+  }
+
+  async openWebPage() {
+    try {
+      await Browser.open({ url: 'https://final-project-dam.netlify.app'});
+    } catch (error) {
+      console.error('Error al abrir la página web', error);
+    }
+  }
 
 }
